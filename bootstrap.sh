c5ea60f61c38e3c0b56b3a17fa03a34d9329c99b
Marvin Pinto <marvin@pinto.im>
2016-02-10 19:00:26 -0500

    Make the apt-get updates less verbose

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,12 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "Installing git"
 sudo apt-add-repository -y ppa:git-core/ppa
-sudo apt-get update
+sudo apt-get -qq update
 sudo apt-get -y install git
 
 echo "Installing ansible"
 sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
+sudo apt-get -qq update
 sudo apt-get install -y ansible
 
 echo "Cloning https://github.com/marvinpinto/dotfiles.git"
@@ -26,6 +26,6 @@ make dotfiles
 
 echo "Upgrading Ubuntu"
 cd /tmp
-sudo apt-get update
+sudo apt-get -qq update
 sudo apt-get -y dist-upgrade
 sudo apt-get autoremove -y --purge
