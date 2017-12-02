10502e8fe67c763649a08c865722184a98b76785
Marvin Pinto <git@pinto.im>
2017-12-01 22:46:35 -0500

    Explicitly use gpg2 as we no longer have a global symlink in /usr/local/bin

--- vault_pass.sh
+++ vault_pass.sh
@@ -2,5 +2,5 @@
 if [ -n "$ANSIBLE_VAULT_PASSWORD" ]; then
   printf "%s" "$ANSIBLE_VAULT_PASSWORD"
 else
-  gpg --batch --use-agent --decrypt vault_passphrase.gpg
+  gpg2 --batch --use-agent --decrypt vault_passphrase.gpg
 fi
