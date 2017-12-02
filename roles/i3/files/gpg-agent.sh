097ee52dbd511cbbbe400d3a5ea1ea712e845c2f
Marvin Pinto <git@pinto.im>
2017-12-01 20:16:03 -0500

    Update the init files to account for gpg 2.1 changes
    More info: https://www.gnupg.org/faq/whats-new-in-2.1.html

--- roles/i3/files/gpg-agent.sh
+++ roles/i3/files/gpg-agent.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Start the GnuPG agent and enable OpenSSH agent emulation
-gnupginf="${HOME}/.gnupg/gpg-agent-info"
-
-if pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
-    eval `cat $gnupginf`
-    eval `cut -d= -f1 $gnupginf | xargs echo export`
+if [ "$(lsb_release --release --short)" == "16.04" ]; then
+  gpgconf --launch gpg-agent
 else
-    eval `gpg-agent --enable-ssh-support --sh --daemon --write-env-file "$gnupginf"`
+  # Start the GnuPG agent and enable OpenSSH agent emulation
+  gnupginf="${HOME}/.gnupg/gpg-agent-info"
+
+  if pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
+      eval `cat $gnupginf`
+      eval `cut -d= -f1 $gnupginf | xargs echo export`
+  else
+      eval `gpg-agent --enable-ssh-support --sh --daemon --write-env-file "$gnupginf"`
+  fi
 fi
