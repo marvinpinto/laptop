8adc809bf75a42785c4db42ae1a53dfb7438a7c5
Marvin Pinto <git@pinto.im>
2018-10-14 09:38:08 -0400

    Start chromium with specific profiles in the different workspaces

--- roles/i3/files/restore-layout.sh
+++ roles/i3/files/restore-layout.sh
@@ -5,17 +5,17 @@ SLEEP_DURATION=2
 notify-send -i info -u normal -t 1000 -- 'Restoring basic window layout'
 i3-msg 'workspace primary; exec xterm -en utf-8;'
 sleep ${SLEEP_DURATION}
-i3-msg 'workspace research; exec chromium-browser;'
+i3-msg 'workspace research; exec chromium-browser --profile-directory="Default";'
 sleep ${SLEEP_DURATION}
-i3-msg 'workspace sb; exec xterm -en utf-8;'
+i3-msg 'workspace sb; exec chromium-browser --profile-directory="Profile 2";'
 sleep ${SLEEP_DURATION}
-i3-msg 'workspace browser; exec chromium-browser;'
+i3-msg 'workspace browser; exec chromium-browser --profile-directory="Default";'
 sleep ${SLEEP_DURATION}
 i3-msg 'workspace sbchrome; exec google-chrome;'
 sleep ${SLEEP_DURATION}
 i3-msg 'workspace irc; exec xterm -en utf-8;'
 sleep ${SLEEP_DURATION}
-i3-msg 'workspace social; exec chromium-browser;'
+i3-msg 'workspace social; exec chromium-browser --profile-directory="Default";'
 sleep ${SLEEP_DURATION}
 i3-msg 'workspace primary;'
 sleep ${SLEEP_DURATION}
