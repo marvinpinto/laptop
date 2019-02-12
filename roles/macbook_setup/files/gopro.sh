543e2664396c913aee92fb2f1b4f9e204cc2d61c
Marvin Pinto <git@pinto.im>
2019-02-12 09:04:31 -0500

    Tweak the video option to only process single files

--- roles/macbook_setup/files/gopro.sh
+++ roles/macbook_setup/files/gopro.sh
@@ -4,7 +4,7 @@ set -o pipefail
 set -e
 
 myname=`basename "$0"`
-video_files=""
+video_file=""
 image_file=""
 live_run=""
 EXIFTOOL=""
@@ -16,7 +16,7 @@ join_video_args=""
 show_help() {
   echo "usage: ${myname} OPTIONS"
   echo "Available Options:"
-  echo "-v <video files>: Process videos"
+  echo "-v <video file>: Process & fix a single video file"
   echo "   Environment variables & defaults:"
   echo "     - OUTPUT_RESOLUTION: <option> <default: 1080p>"
   echo "        - 1440p: 1920x1440"
@@ -25,7 +25,6 @@ show_help() {
   echo "     - ENABLE_VIDEO_STABILIZATION: yes|<empty> <default: empty>"
   echo "     - VIDEO_STABILIZATION_SMOOTHING_FACTOR: 10"
   echo "     - VIDEO_STABILIZATION_SHAKINESS_FACTOR: 5"
-  echo "     - METADATA_FILE: <defaults to first supplied video arg>"
   echo "     - VERBOSE: yes|<empty> <default: empty>"
   echo "     - ENABLE_FISHEYE: yes|<empty> <default: yes>"
   echo "     - CURVES_PRESET_FILTER: <option> <default: none>"
@@ -40,6 +39,8 @@ show_help() {
   echo "        - negative"
   echo "        - strong_contrast"
   echo "        - vintage"
+  echo "     - VIDEO_SAMPLE_TIME: N seconds <default: 15>"
+  echo "     - LIVE_RUN: yes|<empty> <default: empty>"
   echo "-i <single image file | ALL>: Process images"
   echo "   The string \"ALL\" will process all *.jpg images in the current directory."
   echo "   Environment variables & defaults:"
@@ -61,11 +62,7 @@ show_help() {
 
 while getopts ":v:i:lc:s:j:" opt; do
   case "$opt" in
-    v) video_files=("$OPTARG")
-       until [[ $(eval "echo \${$OPTIND}") =~ ^-.* ]] || [ -z $(eval "echo \${$OPTIND}") ]; do
-           video_files+=($(eval "echo \${$OPTIND}"))
-           OPTIND=$((OPTIND + 1))
-       done
+    v) video_file=$OPTARG
        ;;
     c) video_clip_args=("$OPTARG")
        until [[ $(eval "echo \${$OPTIND}") =~ ^-.* ]] || [ -z $(eval "echo \${$OPTIND}") ]; do
@@ -119,16 +116,19 @@ hash sox 2>/dev/null || { echo >&2 "sox does not appear to be available."; exit 
 [[ -z "$verbose" ]] && EXIFTOOL="exiftool -ignoreMinorErrors -q -q" || EXIFTOOL="exiftool"
 
 process_single_video() {
-  local file=$1
-  local temp_video_dir=$2
-  local original_filename=$(basename -- "$file")
-  local sample_time_seconds=15
-  local  __resultvar=$3
+  local temp_video_dir=${myname}-temp-videos
+  rm -rf "$temp_video_dir"
+  mkdir -p "$temp_video_dir"
+
+  local original_filename=$(basename -- "$video_file")
+  local sample_time_seconds=${VIDEO_SAMPLE_TIME:-15}
   local output_resolution=${OUTPUT_RESOLUTION:-1080p}
   local video_stabilization_smoothing_factor=${VIDEO_STABILIZATION_SMOOTHING_FACTOR:-10}
   local video_stabilization_shakiness_factor=${VIDEO_STABILIZATION_SHAKINESS_FACTOR:-5}
   local enable_fisheye=${ENABLE_FISHEYE:-"yes"}
   local curves_preset_filter=${CURVES_PRESET_FILTER:-none}
+
+  $EXIFTOOL -overwrite_original -tagsfromfile "${video_file}" "${temp_video_dir}/metadata-info.mie"
 
   local ffmpeg_requested_width=""
   local ffmpeg_requested_height=""
@@ -143,10 +143,10 @@ process_single_video() {
     ffmpeg_requested_height="720"
   fi
 
-  echo "- Processing video file: ${file}"
-  echo "  - Creating working copy"
+  echo "- Processing video file: ${video_file}"
+  echo "- Creating working copy"
   local renamed_file=${original_filename// /_}
-  cp "${file}" "${temp_video_dir}/${renamed_file}"
+  cp "${video_file}" "${temp_video_dir}/${renamed_file}"
 
   local filename=$(basename -- "$renamed_file")
   local extension="${filename##*.}"
@@ -154,52 +154,52 @@ process_single_video() {
 
   local video_stabilization_filter=""
   if [[ -n "$ENABLE_VIDEO_STABILIZATION" ]]; then
-    echo "  - Initiating video stabilization"
+    echo "- Initiating video stabilization"
     local vidstabtrf_args=()
     [[ -z "$verbose" ]] && vidstabtrf_args+=(-loglevel fatal) || vidstabtrf_args+=(-loglevel info)
-    [[ -z "$live_run" ]] && vidstabtrf_args+=(-y)
+    [[ -z "$LIVE_RUN" ]] && vidstabtrf_args+=(-y)
     vidstabtrf_args+=(-threads $(nproc --ignore=1))
     vidstabtrf_args+=(-i "${temp_video_dir}/${filename}.${extension}")
-    [[ -z "$live_run" ]] && vidstabtrf_args+=(-t $sample_time_seconds)
+    [[ -z "$LIVE_RUN" ]] && vidstabtrf_args+=(-t $sample_time_seconds)
     vidstabtrf_args+=(-vf "vidstabdetect=shakiness=${video_stabilization_shakiness_factor}:result=${temp_video_dir}/${filename}-transform_vectors.trf")
     vidstabtrf_args+=(-f null -)
     ffmpeg2 "${vidstabtrf_args[@]}"
 
-    video_stabilization_filter=",vidstabtransform=input=${temp_video_dir}/${filename}-transform_vectors.trf:zoom=0:smoothing=${video_stabilization_smoothing_factor},unsharp"
+    video_stabilization_filter="vidstabtransform=input=${temp_video_dir}/${filename}-transform_vectors.trf:zoom=0:smoothing=${video_stabilization_smoothing_factor},unsharp,"
   fi
 
-  local ffmpeg_video_aspect_ratio=$(ffprobe -v error -select_streams v:0 -show_entries stream=display_aspect_ratio -of default=nw=1:nk=1 ${file})
-  local ffmpeg_video_width=$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of default=nw=1:nk=1 ${file})
-  local ffmpeg_video_height=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=nw=1:nk=1 ${file})
+  local ffmpeg_video_aspect_ratio=$(ffprobe -v error -select_streams v:0 -show_entries stream=display_aspect_ratio -of default=nw=1:nk=1 ${video_file})
+  local ffmpeg_video_width=$(ffprobe -v error -select_streams v:0 -show_entries stream=width -of default=nw=1:nk=1 ${video_file})
+  local ffmpeg_video_height=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=nw=1:nk=1 ${video_file})
 
   # If the input video's aspect ratio is already 16x9, scale it down to the requested (resolution) height (if the requested height is less than the current height, since we only scale down) - e.g. scale=-2:'min(REQUESTED_HEIGHT,ih)'
   # If the input video's aspect ratio is *not* 16:9, crop, then scale it to the requested size.
   # The second option will throw an error if the input video is less than the requested resolution, since we only scale down.
   local ffmpeg_resolution_filter=""
   if [[ "$ffmpeg_video_aspect_ratio" == "16:9" ]]; then
-    echo "  - Downscaling file to ${ffmpeg_requested_width}x${ffmpeg_requested_height}"
+    echo "- Downscaling file to ${ffmpeg_requested_width}x${ffmpeg_requested_height}"
     ffmpeg_resolution_filter="scale=-2:'min(${ffmpeg_requested_height},ih)'"
   else
     if [[ $ffmpeg_requested_height -gt $ffmpeg_video_height ]]; then
-      echo "Error: Requested resolution of ${ffmpeg_requested_width}x${ffmpeg_requested_height} is larger resolution of ${file} (${ffmpeg_video_width}x${ffmpeg_video_height})."
+      echo "Error: Requested resolution of ${ffmpeg_requested_width}x${ffmpeg_requested_height} is larger resolution of ${video_file} (${ffmpeg_video_width}x${ffmpeg_video_height})."
       exit 1
     fi
 
-    echo "  - Cropping & downscaling file to ${ffmpeg_requested_width}x${ffmpeg_requested_height}"
+    echo "- Cropping & downscaling file to ${ffmpeg_requested_width}x${ffmpeg_requested_height}"
     ffmpeg_resolution_filter="crop=w=${ffmpeg_requested_width}:h=${ffmpeg_requested_height},scale=-2:${ffmpeg_requested_height}"
   fi
 
-  echo "  - Re-encoding video"
-  local base_video_filter="${ffmpeg_resolution_filter},curves=preset='${curves_preset_filter}'${video_stabilization_filter}"
+  echo "- Re-encoding video"
+  local base_video_filter="${video_stabilization_filter}${ffmpeg_resolution_filter},curves=preset='${curves_preset_filter}'"
   local reencode_output_filename_type=""
-  [[ -z "$live_run" ]] && reencode_output_filename_type="sample"
-  [[ -n "$live_run" ]] && reencode_output_filename_type="reencoded"
+  [[ -z "$LIVE_RUN" ]] && reencode_output_filename_type="sample"
+  [[ -n "$LIVE_RUN" ]] && reencode_output_filename_type="reencoded"
   reencode_args=()
   [[ -z "$verbose" ]] && reencode_args+=(-loglevel fatal) || reencode_args+=(-loglevel info)
-  [[ -z "$live_run" ]] && reencode_args+=(-y)
+  [[ -z "$LIVE_RUN" ]] && reencode_args+=(-y)
   reencode_args+=(-threads $(nproc --ignore=1))
   reencode_args+=(-i "${temp_video_dir}/${filename}.${extension}")
-  [[ -z "$live_run" ]] && reencode_args+=(-t $sample_time_seconds)
+  [[ -z "$LIVE_RUN" ]] && reencode_args+=(-t $sample_time_seconds)
   [[ -n "$enable_fisheye" ]] && reencode_args+=(-vf "${base_video_filter},lenscorrection=k1=-0.227:k2=-0.022") || reencode_args+=(-vf "${base_video_filter}")
   reencode_args+=(-vcodec libx264)
   reencode_args+=(-acodec copy)
@@ -208,8 +208,8 @@ process_single_video() {
   reencode_args+=("${temp_video_dir}/${filename}-${reencode_output_filename_type}.mp4")
   ffmpeg2 "${reencode_args[@]}"
 
-  if [[ -z "$live_run" ]]; then
-    echo "  - Generating side-by-side sample"
+  if [[ -z "$LIVE_RUN" ]]; then
+    echo "- Generating side-by-side sample"
     local sidebyside_args=()
     [[ -z "$verbose" ]] && sidebyside_args+=(-loglevel fatal) || sidebyside_args+=(-loglevel info)
     sidebyside_args+=(-y)
@@ -225,72 +225,20 @@ process_single_video() {
     ffmpeg2 "${sidebyside_args[@]}"
   fi
 
-  if [[ -n "$live_run" ]]; then
-    echo "  - Deleting original file: ${file}"
-    rm -f "${file}"
-  fi
-
-  eval $__resultvar="'${filename}'"
-}
-
-process_videos() {
-  local temp_video_dir=${myname}-temp-videos
-  rm -rf "$temp_video_dir"
-  mkdir -p "$temp_video_dir"
-
-  local ffmpeg_input_args=()
-  local ffmpeg_filter_str=()
-  local CTR=0
-  local processed_video_filenames=()
-  local video_metadata_file=${METADATA_FILE:-${video_files[0]}}
-
-  $EXIFTOOL -overwrite_original -tagsfromfile "${video_metadata_file}" "${temp_video_dir}/metadata-info.mie"
-
-  local reencode_output_filename_type=""
-  [[ -z "$live_run" ]] && reencode_output_filename_type="sample"
-  [[ -n "$live_run" ]] && reencode_output_filename_type="reencoded"
-
-  [[ -z "$verbose" ]] && ffmpeg_input_args+=(-loglevel fatal) || ffmpeg_input_args+=(-loglevel info)
-  [[ -z "$live_run" ]] && ffmpeg_input_args+=(-y)
-  ffmpeg_input_args+=(-threads $(nproc --ignore=1))
-
-  for video in "${video_files[@]}"; do
-    process_single_video "$video" "$temp_video_dir" RESULT_VAR
-
-    ffmpeg_input_args+=(-i "${temp_video_dir}/${RESULT_VAR}-${reencode_output_filename_type}.mp4")
-    ffmpeg_filter_str+=([${CTR}:v:0][${CTR}:a:0])
-    CTR=$((CTR+1))
-
-    echo "  - EXIF metadata"
-    find ${temp_video_dir}/ -maxdepth 1 -iname "${RESULT_VAR}*.mp4" | while read file; do
-      $EXIFTOOL -overwrite_original -tagsfromfile "${temp_video_dir}/metadata-info.mie" "${file}"
-      set +e
-      $EXIFTOOL -overwrite_original '-datetimeoriginal<CreateDate' -if '(not $datetimeoriginal or ($datetimeoriginal eq "0000:00:00 00:00:00"))' "${file}"
-      set -e
-    done
-
-    processed_video_filenames+=($RESULT_VAR)
+  echo "- EXIF metadata"
+  find ${temp_video_dir}/ -maxdepth 1 -iname "${filename}*.mp4" | while read file; do
+    $EXIFTOOL -overwrite_original -tagsfromfile "${temp_video_dir}/metadata-info.mie" "${file}"
+    set +e
+    $EXIFTOOL -overwrite_original '-datetimeoriginal<CreateDate' -if '(not $datetimeoriginal or ($datetimeoriginal eq "0000:00:00 00:00:00"))' "${file}"
+    set -e
   done
-
-  local filter_str="$(echo -e "${ffmpeg_filter_str[@]}" | tr -d '[:space:]')"
-  filter_str+="concat=n=${CTR}:v=1:a=1[outv][outa]"
-
-  echo "- Finalizing concatenated ouput"
-  ffmpeg2 ${ffmpeg_input_args[@]} -filter_complex "${filter_str}" -map "[outv]" -map "[outa]" ${temp_video_dir}/concatenated-output.mp4
-  $EXIFTOOL -overwrite_original -tagsfromfile "${temp_video_dir}/metadata-info.mie" "${temp_video_dir}/concatenated-output.mp4"
-  set +e
-  $EXIFTOOL -overwrite_original '-datetimeoriginal<CreateDate' -if '(not $datetimeoriginal or ($datetimeoriginal eq "0000:00:00 00:00:00"))' "${temp_video_dir}/concatenated-output.mp4"
-  set -e
-  [[ -z "$live_run" ]] &&  $EXIFTOOL  -overwrite_original '-FileName<DateTimeOriginal' -if '($datetimeoriginal)' -d "%Y-%m-%d_%H.%M.%S%%-c-concatenated-output.%%le" "${temp_video_dir}/concatenated-output.mp4"
 
   echo "- Renaming re-encoded outputs"
-  for video in "${processed_video_filenames[@]}"; do
-    find "${temp_video_dir}/" -iname "${video}.mp4" -exec $EXIFTOOL -overwrite_original '-FileName<DateTimeOriginal' -if '($datetimeoriginal)' -d "%Y-%m-%d_%H.%M.%S%%-c-${video}.%%le" {} \;
-    [[ -z "$live_run" ]] && $EXIFTOOL  -overwrite_original '-FileName<DateTimeOriginal' -if '($datetimeoriginal)' -d "%Y-%m-%d_%H.%M.%S%%-c-${video}-${reencode_output_filename_type}.%%le" "${temp_video_dir}/${video}-${reencode_output_filename_type}.mp4"
-    [[ -z "$live_run" ]] && $EXIFTOOL  -overwrite_original '-FileName<DateTimeOriginal' -if '($datetimeoriginal)' -d "%Y-%m-%d_%H.%M.%S%%-c-${video}-combined.%%le" "${temp_video_dir}/${video}-combined.mp4"
-  done
+  find "${temp_video_dir}/" -iname "${filename}.mp4" -exec $EXIFTOOL -overwrite_original '-FileName<DateTimeOriginal' -if '($datetimeoriginal)' -d "%Y-%m-%d_%H.%M.%S%%-c-${filename}.%%le" {} \;
+  [[ -z "$LIVE_RUN" ]] && $EXIFTOOL  -overwrite_original '-FileName<DateTimeOriginal' -if '($datetimeoriginal)' -d "%Y-%m-%d_%H.%M.%S%%-c-${filename}-${reencode_output_filename_type}.%%le" "${temp_video_dir}/${filename}-${reencode_output_filename_type}.mp4"
+  [[ -z "$LIVE_RUN" ]] && $EXIFTOOL  -overwrite_original '-FileName<DateTimeOriginal' -if '($datetimeoriginal)' -d "%Y-%m-%d_%H.%M.%S%%-c-${filename}-combined.%%le" "${temp_video_dir}/${filename}-combined.mp4"
 
-  if [[ -z "$live_run" ]]; then
+  if [[ -z "$LIVE_RUN" ]]; then
     echo -ne "\n"
     echo "****************************"
     echo " Sample Generation Complete "
@@ -300,10 +248,10 @@ process_videos() {
     echo "If everything looks good, re-run this script with the -l flag."
   fi
 
-  if [[ -n "$live_run" ]]; then
+  if [[ -n "$LIVE_RUN" ]]; then
     echo "- Final cleanup"
-    mv "${temp_video_dir}/concatenated-output.mp4" .
-    $EXIFTOOL  -overwrite_original '-FileName<DateTimeOriginal' -if '($datetimeoriginal)' -d "%Y-%m-%d_%H.%M.%S%%-c.%%le" "./concatenated-output.mp4"
+    mv ${temp_video_dir}/*${filename}-${reencode_output_filename_type}.mp4 .
+    $EXIFTOOL  -overwrite_original '-FileName<DateTimeOriginal' -if '($datetimeoriginal)' -d "%Y-%m-%d_%H.%M.%S%%-c.%%le" *${filename}-${reencode_output_filename_type}.mp4
     rm -rf "$temp_video_dir"
   fi
 }
@@ -657,8 +605,8 @@ join_video_files() {
   rm -rf "$temp_video_dir"
 }
 
-if [[ -n "$video_files" ]]; then
-  process_videos
+if [[ -n "$video_file" ]]; then
+  process_single_video
 elif [[ -n "$image_file" ]]; then
   process_images
 elif [[ -n "$video_clip_args" ]]; then
