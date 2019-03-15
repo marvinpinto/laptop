2eaff095fb6db30fd1a223faa148356e1706eaeb
Marvin Pinto <git@pinto.im>
2019-03-15 19:02:11 -0400

    Move the "scratch" type apps into a floating tmux window

---
+++ roles/i3/files/tmux-startup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+SESSION_NAME="scratch"
+
+tmux kill-session -t "${SESSION_NAME}" > /dev/null 2>&1
+tmux new-session -d -s "${SESSION_NAME}" "im"
+tmux rename-window "irssi"
+tmux new-window "vi ~/scratchpad.md"
+tmux rename-window "notes"
+tmux new-window
+tmux rename-window "files"
+tmux new-window
+tmux rename-window "workspace"
+tmux select-window -t "irssi"
+tmux attach-session -d
