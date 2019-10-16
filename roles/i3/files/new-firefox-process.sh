360ce45a3a71d5208871257f3708cf23bbc8ec50
Marvin Pinto <git@pinto.im>
2019-10-16 15:37:37 -0400

    Revert "Add an item to launch a new separate firefox process"
    This reverts commit 4d9da6a68abaca5a81bb46388f5082a29c447510.

--- roles/i3/files/new-firefox-process.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-PROFILEDIR=$(mktemp -p /tmp -d tmp-fx-profile.XXXXXX.d)
-DEFAULT_PROFILE=$(grep "Default=.*default-.*" ${HOME}/.mozilla/firefox/profiles.ini | cut -d = -f2)
-
-cp -R ${HOME}/.mozilla/firefox/${DEFAULT_PROFILE}/* ${PROFILEDIR}/
-
-/usr/bin/firefox -profile $PROFILEDIR --no-remote --private-window
-rm -rf "$PROFILEDIR"
--- roles/macbook_setup/files/rofi-launcher.sh
+++ roles/macbook_setup/files/rofi-launcher.sh
@@ -6,7 +6,7 @@ I3LOCK='/usr/bin/i3lock --color 333333'
 if [ -z $@ ]; then
   function gen_options()
   {
-    option_list=("Lock Screen","Logout","Suspend Computer","Hibernate Computer","Reboot Computer","Shutdown Computer","Enable Screensaver","Disable Screensaver","Toggle Do Not Disturb (DND)","Initiate Backup","Check Email","Create Development Workspace","2FA Tokens","Emoji Picker","Switch Bluetooth Device","New Firefox Process")
+    option_list=("Lock Screen","Logout","Suspend Computer","Hibernate Computer","Reboot Computer","Shutdown Computer","Enable Screensaver","Disable Screensaver","Toggle Do Not Disturb (DND)","Initiate Backup","Check Email","Create Development Workspace","2FA Tokens","Emoji Picker","Switch Bluetooth Device")
     echo ${option_list[@]} | tr ',' '\n' | sort
   }
 
@@ -63,9 +63,6 @@ else
       ;;
     'Switch Bluetooth Device')
       coproc (/usr/local/bin/switch-bluetooth-device)
-      ;;
-    'New Firefox Process')
-      coproc (${HOME}/.i3/new-firefox-process.sh)
       ;;
     *)
       echo "Invalid option \"$OPTION\""
